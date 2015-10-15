6b468603192b76879b99863903480e37215cfb82
pb-sliang <sliang@pacb.com>
2015-10-15 15:51:56 -0700

    fix genome_size option

--- tap.sh
+++ tap.sh
@@ -1,11 +1,34 @@
-#!/bin/sh
+#!/bin/bash
 
-dir=`pwd`
-if [ ! -f $dir/genome_size.txt ] ; then
-    echo "no genome_size file"
-    exit 1
+usage () { echo "Usage: $0 -g genome_size_in_MB"; exit 1;}
+
+while getopts "hg:" o ; do
+   case "${o}" in
+       h)
+           usage
+           ;;
+       g)
+           genome_size_in_mb=${OPTARG}
+           ;;
+       *)
+           usage
+           ;;
+    esac
+done
+
+if [ -z "$genome_size_in_mb" ]; then
+    dir=`pwd`
+    if [ ! -f $dir/genome_size.txt ] ; then
+        echo "no genome_size file"
+        exit 1
+    fi
+    genome_size_in_mb=`cat genome_size.txt`
 fi
-genome_size_in_mb=`cat genome_size.txt`
+
+if [ "$genome_size_in_mb" == 0. ]; then
+echo "Usage: $0 -g genome_size_in_MB\n OR set a genome_size.txt file"; exit 1;
+fi
+
 
 # run in a dir from falcon
 db=0-rawreads/raw_reads.db
